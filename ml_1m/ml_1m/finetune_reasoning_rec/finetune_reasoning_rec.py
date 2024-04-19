3ea8880b7d851984e900081b27d6df4e1153e1d4
Millennium <millennium.milan1@gmail.com>
2024-04-19 15:56:03 -0500

    Final change to train and valid data

--- ml_1m/ml_1m/finetune_reasoning_rec/finetune_reasoning_rec.py
+++ ml_1m/ml_1m/finetune_reasoning_rec/finetune_reasoning_rec.py
@@ -90,7 +90,7 @@ def train(
     # print(f"gradient_accumulation_steps: {gradient_accumulation_steps}")
 
     device_map = 'auto'
-    max_memory_mapping = {0: "10GiB", 1: "10GiB", 2: "10GiB", 3: "10GiB"}
+    max_memory_mapping = {0: "23GiB", 1: "23GiB"}
     world_size = int(os.environ.get("WORLD_SIZE", 1))
     ddp = world_size != 1
     if ddp:
@@ -236,7 +236,7 @@ def train(
         Original Trainer may have a memory leak. 
         This is a workaround to avoid storing too many tensors that are not needed.
         """
-        # print(f"len of logits: {logits.shape}")
+        print(f"len of logits: {logits.shape}")
         # print(f"logits dimension: {len(logits[0])}")
         # print(f"len of labels: {labels.shape} --- labels: {labels}")
         # print(f"labels dimension: {len(labels[0])}")
@@ -254,7 +254,7 @@ def train(
 
         # print("labels:", labels)
         # print("gts:", gts)
-        # print("GT:", tokenizer.batch_decode(gts, skip_special_tokens=False, clean_up_tokenization_spaces=True))
+        print("GT:", tokenizer.batch_decode(gts, skip_special_tokens=False, clean_up_tokenization_spaces=True))
 
         labels_index = torch.argwhere(torch.bitwise_or(labels == 8241, labels == 3782))
         gold = torch.where(labels[labels_index[:, 0], labels_index[:, 1]] == 3782, 0, 1)
@@ -262,7 +262,8 @@ def train(
         # logits = logits.softmax(dim=-1)
         argmax_indices = torch.argmax(logits, dim=-1)
         # print("Predicted Indices:", argmax_indices)
-        # print("Predicted:", tokenizer.batch_decode(argmax_indices, skip_special_tokens=False, clean_up_tokenization_spaces=True))
+        print("-"*100)
+        print("Predicted:", tokenizer.batch_decode(argmax_indices, skip_special_tokens=False, clean_up_tokenization_spaces=True))
         logits = torch.softmax(logits[labels_index[:, 0], labels_index[:, 1]][:,[3782, 8241]], dim = -1)
         # print(logits)
         # print(logits[:, 1][2::3], gold[2::3])
@@ -277,8 +278,58 @@ def train(
             eval_step = sample / 128 * 2
     # print("sample: ", sample)
     
-    # trainer = transformers.Trainer(
-    #     model=model,
+    trainer = transformers.Trainer(
+        model=model,
+        train_dataset=train_data,
+        eval_dataset=val_data,
+        args=transformers.TrainingArguments(
+            per_device_train_batch_size=micro_batch_size,
+            gradient_accumulation_steps=gradient_accumulation_steps,
+            warmup_steps=20,
+            num_train_epochs=num_epochs,
+            learning_rate=learning_rate,
+            fp16=True,
+            logging_steps=20,
+            optim="adamw_torch",
+            evaluation_strategy="steps",
+            save_strategy="steps",
+            eval_steps=eval_step,
+            save_steps=eval_step,
+            output_dir=output_dir,
+            save_total_limit=1,
+            load_best_model_at_end=True,
+            metric_for_best_model="eval_auc",
+            ddp_find_unused_parameters=False if ddp else None,
+            group_by_length=group_by_length,
+            report_to=None,
+            # report_to="wandb" if use_wandb else None,
+            # run_name=wandb_run_name if use_wandb else None,
+            # eval_accumulation_steps=10,
+        ),
+        data_collator=transformers.DataCollatorForSeq2Seq(
+            tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+        ),
+        # data_collator=transformers.DataCollatorForLanguageModeling(
+        #     tokenizer, pad_to_multiple_of=8, return_tensors="pt", mlm=False,
+        # ),
+        compute_metrics=compute_metrics,
+        preprocess_logits_for_metrics=preprocess_logits_for_metrics,
+        callbacks = [EarlyStoppingCallback(early_stopping_patience=3)]
+    )
+
+    ### Using SFTTrainer
+    # def formatting_prompts_func(example):
+    #     output_texts = []
+    #     for i in range(len(example['instruction'])):
+    #         text = f"### Instruction: {example['instruction'][i]}\n ### Input: {example['input'][i]}\n ### Response: {example['output'][i]}"
+    #         output_texts.append(text)
+    #     return output_texts
+
+    # response_template = "### Response:"
+    # collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer, mlm=False)
+
+    # trainer = SFTTrainer(
+    #     model,
     #     train_dataset=train_data,
     #     eval_dataset=val_data,
     #     args=transformers.TrainingArguments(
@@ -308,66 +359,16 @@ def train(
     #     # data_collator=transformers.DataCollatorForSeq2Seq(
     #     #     tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
     #     # ),
-    #     data_collator=transformers.DataCollatorForLanguageModeling(
-    #         tokenizer, pad_to_multiple_of=8, return_tensors="pt", mlm=False,
-    #     ),
+    #     # data_collator=transformers.DataCollatorForLanguageModeling(
+    #     #     tokenizer, pad_to_multiple_of=8, return_tensors="pt", mlm=False,
+    #     # ),
+    #     max_seq_length = 2048,
+    #     formatting_func = formatting_prompts_func,
+    #     data_collator = collator,
     #     compute_metrics=compute_metrics,
     #     preprocess_logits_for_metrics=preprocess_logits_for_metrics,
     #     callbacks = [EarlyStoppingCallback(early_stopping_patience=3)]
     # )
-
-    ### Using SFTTrainer
-    def formatting_prompts_func(example):
-        output_texts = []
-        for i in range(len(example['instruction'])):
-            text = f"### Instruction: {example['instruction'][i]}\n ### Input: {example['input'][i]}\n ### Response: {example['output'][i]}"
-            output_texts.append(text)
-        return output_texts
-
-    response_template = "### Response:"
-    collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer, mlm=False)
-
-    trainer = SFTTrainer(
-        model,
-        train_dataset=train_data,
-        eval_dataset=val_data,
-        args=transformers.TrainingArguments(
-            per_device_train_batch_size=micro_batch_size,
-            gradient_accumulation_steps=gradient_accumulation_steps,
-            warmup_steps=20,
-            num_train_epochs=num_epochs,
-            learning_rate=learning_rate,
-            fp16=True,
-            logging_steps=20,
-            optim="adamw_torch",
-            evaluation_strategy="steps",
-            save_strategy="steps",
-            eval_steps=eval_step,
-            save_steps=eval_step,
-            output_dir=output_dir,
-            save_total_limit=1,
-            load_best_model_at_end=True,
-            metric_for_best_model="eval_auc",
-            ddp_find_unused_parameters=False if ddp else None,
-            group_by_length=group_by_length,
-            report_to=None,
-            # report_to="wandb" if use_wandb else None,
-            # run_name=wandb_run_name if use_wandb else None,
-            # eval_accumulation_steps=10,
-        ),
-        # data_collator=transformers.DataCollatorForSeq2Seq(
-        #     tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
-        # ),
-        # data_collator=transformers.DataCollatorForLanguageModeling(
-        #     tokenizer, pad_to_multiple_of=8, return_tensors="pt", mlm=False,
-        # ),
-        max_seq_length = 2048,
-        formatting_func = formatting_prompts_func,
-        data_collator = collator,
-        compute_metrics=compute_metrics,
-        preprocess_logits_for_metrics=preprocess_logits_for_metrics,
-        callbacks = [EarlyStoppingCallback(early_stopping_patience=3)]
-    )
     model.config.use_cache = False
 
     # old_state_dict = model.state_dict
